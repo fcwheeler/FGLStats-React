29a35bb6cf50c55b507470e0a81cef446c22c30a
fcwheeler <fcwheeler@gmail.com>
2019-02-24 22:29:42 -0500

    Set width for Leaderboard page

--- src/views/Leaderboard.js
+++ src/views/Leaderboard.js
@@ -1,43 +1,41 @@
-import React, { Component } from 'react';
-import { withStyles } from '@material-ui/core/styles';
+import React, { Component } from "react";
+import { withStyles } from "@material-ui/core/styles";
 import LeaderboardList from "../Components/LeaderboardList";
-import Grid from '@material-ui/core/Grid';
-import Paper from '@material-ui/core/Paper';
+import Grid from "@material-ui/core/Grid";
+import Paper from "@material-ui/core/Paper";
 
 const styles = theme => ({
   root: {
-    flexGrow: 1,
-
-  }, 
-  header:{
-    textAlign: 'center',
-    alignSelf: 'center'
-
+    flexGrow: 1
+  },
+  header: {
+    textAlign: "center",
+    alignSelf: "center"
   }
-
-
 });
 class Leaderboard extends Component {
-
-
   render() {
-    const { classes } = this.props;    
+    const { classes } = this.props;
     return (
       <div>
-      <h1>Leaderboard</h1>
-      <Grid container  direction="row"  justify="center"  alignItems="center" className={classes.root} spacing={16}>
-      <Grid item lg >
-      <Paper className={classes.root}  elevation={1}>
-      <LeaderboardList></LeaderboardList>
-      </Paper>
-      </Grid> 
-      </Grid>
-     
+        <h1>Leaderboard</h1>
+        <Grid
+          container
+          direction="row"
+          justify="center"
+          alignItems="center"
+          className={classes.root}
+          spacing={16}
+        >
+          <Grid item xs={9}>
+            <Paper className={classes.root} elevation={1}>
+              <LeaderboardList />
+            </Paper>
+          </Grid>
+        </Grid>
       </div>
     );
   }
-
-
 }
 
-export default withStyles(styles)(Leaderboard)
+export default withStyles(styles)(Leaderboard);
